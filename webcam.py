d2be8f174a9d38eeda1b83e875ad823981c1e1ca
ChenningTai <50452986+ChenningTai@users.noreply.github.com>
2019-05-09 22:47:26 +0800

    遮罩只把臉露出來了

--- webcam.py
+++ webcam.py
@@ -1,6 +1,6 @@
 import cv2
 import sys
-
+import numpy as np
 cascPath = "haarcascade_frontalface_default.xml"#路徑
 cascPath1 = "haar_eye.xml"
 faceCascade = cv2.CascadeClassifier(cascPath)#用這個分類去找出來有沒有符合
@@ -8,11 +8,11 @@ eyeCascade = cv2.CascadeClassifier(cascPath1)
 
 video_capture = cv2.VideoCapture(1)
 # video_capture = cv2.VideoCapture('skin_example.mov')
-
+frame = video_capture.read()[1]#boolean, frame
+height, width = frame.shape[:2]
 while True:
     # Capture frame-by-frame
     ret, frame = video_capture.read()#boolean, frame
-
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     #-------------
     # h, w = gray.shape
@@ -44,10 +44,16 @@ while True:
         minSize=(30,30),
         flags=cv2.CASCADE_SCALE_IMAGE
     )
+    img = np.zeros((height,width,1), np.uint8)
 
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)#框框(哪張圖,左上角位置,左下角位置,顏色,寬度)
+
+        img = cv2.rectangle(img, (x-10, y-10), (x+w+15, y+h+15), 255, -1)#框框(哪張圖,左上角位置,左下角位置,顏色,寬度)
+        # img = cv2.rectangle(img, (x, y), (x+w, y+h), 255, -1)
+        cv2.imshow('rec', img)
+        # print(type(img))
+        frame = cv2.bitwise_and(frame, frame, mask = img)#位元運算
     for (x, y, w, h) in eyes:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
     # Display the resulting frame
